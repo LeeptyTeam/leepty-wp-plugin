0d24e6e9e039a68c41192cf216baceedcabc8f1d
Vincent Peybernes [Techniv] <vpeybernes.pro@gmail.com>
2012-07-10 02:58:27 +0200

    Implement search in dictionary.

--- libs/analyser/CommonDictionary.php
+++ libs/analyser/CommonDictionary.php
@@ -8,8 +8,15 @@
 abstract class CommonDictionary {
 	
 	private $words;
+	private $symbole = array(
+		'base' => array('~','"','#','{','}','(',')','[',']','|','%','*','$',
+			'£','¤','§','?',',',';','.',':','/','\\','!','<','>'),
+		
+		'solo'=> array('&','+','#','@')
+	);
 	
-	function __construct() {
+	public function __construct() {
+		$this->symbole['all'] = array_merge($this->symbole['base'], $this->symbole['solo']);
 		$this->words = $this->getWords();
 	}
 
@@ -21,8 +28,31 @@ abstract class CommonDictionary {
 	}
 
 
-	public function search($word){
+	public final function search($word){
+		$first = substr($word, 0,1);
+		if(in_array($first, $this->symbole['all'])){
+			$matchOther = false;
+			$l = strlen($word);
+			for($i=1; $i < $l; $i++){
+				if(!in_array(substr($word, $i,1), $this->symbole['all'])){
+					$matchOther = true;
+					break;
+				}
+			}
+			if(!$matchOther) return true;
+		}
 		return $this->findWord($word);
 	}
 	
+	private static $dictionary = array();
+	public static function getDictionary($lang){
+		$lang = strtolower($lang);
+		if(!isset(self::$dictionary[$lang])){
+			$className = 'dictionary\\'.$lang;
+			$class = new ReflectionClass($className);
+			self::$dictionary[$lang] = $class->newInstance();
+		}
+		
+		return self::$dictionary[$lang];
+	}
 }
--- libs/analyser/testing.php
+++ libs/analyser/testing.php
@@ -2,10 +2,37 @@
 
 include 'LeeptyAnalyser.php';
 
-$class = 'dictionary\\En';
+$class = 'dictionary\\en';
 
 $class = new ReflectionClass($class);
-$dic = $class->newInstance();
+//$dic = $class->newInstance();
+$dic = CommonDictionary::getDictionary('en');
 /* @var $dic CommonDictionary */
 
-var_dump($dic->search('april'));
+var_dump($dic->search('.?.'));
+
+$sample = array(
+	'toto', 'constitusion', 'associated', 'station', 'so', 'sierie', 25, '.', '...', '?', '+a'
+);
+
+$test = 1000000;
+
+$max = count($sample);
+$index = 0;
+
+$t1 = microtime(true);
+for($i=0; $i < $test; $i++){
+	if($index >= $max) $index = 0;
+	
+	$word = $sample[$index];
+	
+	//var_dump($i, $word, $dic->search($word));
+	
+	$index++;
+}
+$t2 = microtime(true);
+$temp = $t2-$t1;
+
+echo 'Nombre de test : '.$test.'<br>';
+echo 'Temps : '.$temp.'s<br>';
+
